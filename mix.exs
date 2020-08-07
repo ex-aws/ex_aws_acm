862575861925d5a860ec0dd90b398ca2e27e7b7a
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-08-07 10:22:29 -0400

    Support elixir 1.8+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAwsAcm.MixProject do
     [
       app: :ex_aws_acm,
       version: "0.0.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
